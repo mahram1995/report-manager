ad656551f6898ce5016b1dc01cda9d90b5529b12
MD Mahram Ali <89145520+mahram1995@users.noreply.github.com>
2025-06-20 11:59:16 +0600

    Initial commit

---
+++ src/main/java/com/mislbd/report_manager/ReportManagerApplication.java
@@ -0,0 +1,14 @@
+package com.mislbd.report_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportManagerApplication.class, args);
+		System.out.println("Hello Report Manager");
+	}
+
+}
---
+++ src/test/java/com/mislbd/report_manager/ReportManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mislbd.report_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
