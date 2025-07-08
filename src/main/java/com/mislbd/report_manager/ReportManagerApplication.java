829dc33b18650a4672bcba0f7fc65dcf566bc801
MD Mahram Ali <89145520+mahram1995@users.noreply.github.com>
2025-07-08 23:43:26 +0600

    add customer specification

--- src/main/java/com/mislbd/report_manager/ReportManagerApplication.java
+++ src/main/java/com/mislbd/report_manager/ReportManagerApplication.java
@@ -2,7 +2,9 @@ package com.mislbd.report_manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ReportManagerApplication  extends SpringBootServletInitializer {
--- src/main/java/com/mislbd/report_manager/configuration/swaggerConfiguration.java
+++ src/main/java/com/mislbd/report_manager/configuration/swaggerConfiguration.java
@@ -2,21 +2,33 @@ package com.mislbd.report_manager.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class swaggerConfiguration {
 
     @Bean
-    public Docket api() {
+    public Docket adminApi() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("Ababil Admin Api" + " V-5.0.01")
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("report_manager.controller"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(
+                        new ApiInfoBuilder()
+                                .title("Ababil Admin Api")
+                                .description("Ababil Admin Api")
+                                .version("5.0.01")
+                                .license("© Millennium Information Solution Limited")
+                                .licenseUrl("http://www.mislbd.com")
+                                .build());
     }
 
 }
