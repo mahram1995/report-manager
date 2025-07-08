4d0cae9a006696659bd58a39b29908dfb3a6976a
mahram <mahram@mislbd.com>
2025-07-08 18:33:14 +0600

    add sqagger configuration

--- src/main/java/com/mislbd/report_manager/ReportManagerApplication.java
+++ src/main/java/com/mislbd/report_manager/ReportManagerApplication.java
@@ -2,9 +2,10 @@ package com.mislbd.report_manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ReportManagerApplication {
+public class ReportManagerApplication  extends SpringBootServletInitializer {
 	// in project structure > project> java-22
 	//  in setting>build>gradle> Java-22
 
--- src/main/java/com/mislbd/report_manager/controller/CustomerController.java
+++ src/main/java/com/mislbd/report_manager/controller/CustomerController.java
@@ -5,9 +5,10 @@ import com.mislbd.report_manager.entity.CustomerEntity;
 import com.mislbd.report_manager.service.CustomerService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
